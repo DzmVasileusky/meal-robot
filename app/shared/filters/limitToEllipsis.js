2784387c53917e7ad9adc4b36a62097cae491f58
Dzmitry Vasilevsky <dzm.vasilevsky@gmail.com>
2017-02-18 13:29:31 +0300

    update limit filter

--- app/shared/filters/limitToEllipsis.js
+++ app/shared/filters/limitToEllipsis.js
@@ -2,8 +2,17 @@
   Limit to filter with ellipsis crop
 ***********************************************/
 angular.module('Shared').filter('limitToEllipsis', function() {
-  return function(input, limit, cropper) {
-    if (!cropper) cropper = '...';
-    return input.length > limit ? input.slice(0, limit) + cropper : input;
+  return function(input, params) {
+    var cropped = input,
+        r;
+    if (!params.cropper) params.cropper = '...';
+
+    if (input.length > params.ammount) {
+      r = new RegExp('\(\^\.\{1,' + params.ammount + '\}\)\\s\.\*');
+      cropped = params.saveWords ? cropped.replace(r,'$1') : input.slice(0, params.ammount);
+      cropped += params.cropper;
+    }
+
+    return cropped;
   };
 });
